6e00ed82ec56613610f1c70ba21954ef4fc25b34
Gerges Nagiub <gerges7gn1@gmail.com>
2022-09-17 04:13:54 +0200

    Completed header, Services section and features section

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+});
+});
