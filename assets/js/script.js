3abd24b9760e72efa10fba2f2dd766965c73c4ae
Gerges Nagiub <gerges7gn1@gmail.com>
2022-09-19 05:45:06 +0200

    Upload Reveiws section and Features 2 section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(function () {
     $(document).scroll(function () {
       var $nav = $(".fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
   });
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+  $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
     disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
+    type: "iframe",
+    mainClass: "mfp-fade",
     removalDelay: 160,
     preloader: false,
-    fixedContentPos: false
-});
+    fixedContentPos: false,
+  });
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+  });
 });
